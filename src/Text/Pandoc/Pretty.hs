a686d468cb62f0627bc7a618567943e32163da80
John MacFarlane <jgm@berkeley.edu>
2019-08-15 17:36:20 -0700

    Pretty - use Text instead of String for better performance.
    Some API changes that need to be ironed out.

--- src/Text/Pandoc/Pretty.hs
+++ src/Text/Pandoc/Pretty.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE CPP                        #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -68,17 +69,15 @@ import Prelude
 import Control.Monad
 import Control.Monad.State.Strict
 import Data.Char (isSpace)
-import Data.Foldable (toList)
 import Data.List (intersperse, foldl')
-import Data.Sequence (Seq, ViewL (..), fromList, singleton, viewl, (<|))
-import qualified Data.Sequence as Seq
 import Data.String
-import qualified Text.DocTemplates as DT
 import qualified Data.Text as T
+import Data.Text (Text)
+import qualified Text.DocTemplates as DT
 
 data RenderState a = RenderState{
          output     :: [a]        -- ^ In reverse order
-       , prefix     :: String
+       , prefix     :: Text
        , usePrefix  :: Bool
        , lineLength :: Maybe Int  -- ^ 'Nothing' means no wrapping
        , column     :: Int
@@ -89,11 +88,11 @@ type DocState a = State (RenderState a) ()
 
 data D a = Text Int a
          | Block Int [a]
-         | Prefixed String (D a)
+         | Prefixed Text (D a)
          | BeforeNonBlank (D a)
          | Flush (D a)
          | BreakingSpace
-         | AfterBreak a
+         | AfterBreak Text
          | CarriageReturn
          | NewLine
          | BlankLines Int  -- number of blank lines
@@ -110,16 +109,16 @@ instance Monoid (D a) where
   mappend = (<>)
   mempty = Empty
 
-type Doc = D String
+type Doc = D Text
 -- newtype Doc = Doc { unDoc :: Seq (D String) }
 --              deriving (Semigroup, Monoid, Show, Eq)
 
 instance IsString a => IsString (D a) where
   fromString = text
 
-instance DT.TemplateTarget (D String) where
+instance DT.TemplateTarget (D Text) where
   fromText = text . T.unpack
-  removeFinalNewline = id
+  removeFinalNewline = chomp
   nested = nest
   isEmpty = isEmpty
 
@@ -219,54 +218,60 @@ chomp d =
           z     -> x <> z
     _                         -> d
 
-outp :: (IsString a) => Int -> String -> DocState a
+outp :: Int -> Text -> DocState Text
 outp off s | off < 0 = do  -- offset < 0 means newline characters
   st' <- get
   let rawpref = prefix st'
-  when (column st' == 0 && usePrefix st' && not (null rawpref)) $ do
-    let pref = reverse $ dropWhile isSpace $ reverse rawpref
-    modify $ \st -> st{ output = fromString pref : output st
+  when (column st' == 0 && usePrefix st' && not (T.null rawpref)) $ do
+    let pref = fromString $ T.unpack $ T.dropWhileEnd isSpace rawpref
+    modify $ \st -> st{ output = pref : output st
                       , column = column st + realLength pref }
-  let numnewlines = length $ takeWhile (=='\n') $ reverse s
-  modify $ \st -> st { output = fromString s : output st
+  modify $ \st -> st { output = s : output st
                      , column = 0
-                     , newlines = newlines st + numnewlines }
+                     }
 outp off s = do           -- offset >= 0 (0 might be combining char)
   st' <- get
-  let pref = prefix st'
-  when (column st' == 0 && usePrefix st' && not (null pref)) $
-    modify $ \st -> st{ output = fromString pref : output st
+  let pref = fromString $ T.unpack $ prefix st'
+  when (column st' == 0 && usePrefix st' && not (T.null pref)) $
+    modify $ \st -> st{ output = pref : output st
                     , column = column st + realLength pref }
-  modify $ \st -> st{ output = fromString s : output st
+  modify $ \st -> st{ output = s : output st
                     , column = column st + off
                     , newlines = 0 }
 
 -- | Renders a 'Doc'.  @render (Just n)@ will use
 -- a line length of @n@ to reflow text on breakable spaces.
 -- @render Nothing@ will not reflow text.
-render :: (IsString a) => Maybe Int -> Doc -> a
-render linelen doc = fromString . mconcat . reverse . output $
+render :: Maybe Int -> D Text -> Text
+render linelen doc = mconcat . reverse . output $
   execState (renderDoc doc) startingState
    where startingState = RenderState{
                             output = mempty
-                          , prefix = ""
+                          , prefix = mempty
                           , usePrefix = True
                           , lineLength = linelen
                           , column = 0
                           , newlines = 2 }
 
-renderDoc :: (IsString a, Monoid a)
-          => Doc -> DocState a
-renderDoc = renderList . dropWhile (== BreakingSpace) . unfoldD
+renderDoc :: D Text -> DocState Text
+renderDoc = renderList . dropWhile isBreakingSpace . unfoldD
+ where
+   isBreakingSpace BreakingSpace = True
+   isBreakingSpace _             = False
 
-data IsBlock = IsBlock Int [String]
+
+data IsBlock = IsBlock Int [Text]
 
 -- This would be nicer with a pattern synonym
 -- pattern VBlock i s <- mkIsBlock -> Just (IsBlock ..)
 
-renderList :: (IsString a, Monoid a)
-           => [D String] -> DocState a
+renderList :: [D Text] -> DocState Text
 renderList [] = return ()
+
+renderList (Concat{} : xs) = renderList xs -- should not happen after unfoldD
+
+renderList (Empty : xs) = renderList xs -- should not happen after unfoldD
+
 renderList (Text off s : xs) = do
   outp off s
   renderList xs
@@ -274,7 +279,7 @@ renderList (Text off s : xs) = do
 renderList (Prefixed pref d : xs) = do
   st <- get
   let oldPref = prefix st
-  put st{ prefix = prefix st ++ pref }
+  put st{ prefix = prefix st <> pref }
   renderDoc d
   modify $ \s -> s{ prefix = oldPref }
   renderList xs
@@ -305,7 +310,9 @@ renderList (BlankLines num : xs) = do
   st <- get
   case output st of
      _ | newlines st > num -> return ()
-       | otherwise -> replicateM_ (1 + num - newlines st) (outp (-1) "\n")
+       | otherwise -> replicateM_ (1 + num - newlines st)
+                        (do outp (-1) "\n"
+                            modify $ \st' -> st'{ newlines = newlines st' + 1 })
   renderList xs
 
 renderList (CarriageReturn : BlankLines m : xs) =
@@ -350,10 +357,11 @@ renderList (BreakingSpace : xs) = do
           outp 1 " "
           renderList xs'
 
-renderList (AfterBreak s : xs) = do
+renderList (AfterBreak t : xs) = do
   st <- get
-  when (newlines st > 0) $ outp (realLength s) s
-  renderList xs
+  if newlines st > 0
+     then renderList (fromString (T.unpack t) : xs)
+     else renderList xs
 
 renderList (Block i1 s1 : Block i2 s2  : xs) =
   renderList (mergeBlocks False (IsBlock i1 s1) (IsBlock i2 s2) : xs)
@@ -365,27 +373,27 @@ renderList (Block _width lns : xs) = do
   st <- get
   let oldPref = prefix st
   case column st - realLength oldPref of
-        n | n > 0 -> modify $ \s -> s{ prefix = oldPref ++ replicate n ' ' }
+        n | n > 0 -> modify $ \s -> s{ prefix = oldPref <> T.replicate n " " }
         _ -> return ()
   renderList $ intersperse CarriageReturn (map (Text 0) lns)
   modify $ \s -> s{ prefix = oldPref }
   renderList xs
 
-mergeBlocks :: Bool -> IsBlock -> IsBlock -> D String
+mergeBlocks :: Bool -> IsBlock -> IsBlock -> D Text
 mergeBlocks addSpace (IsBlock w1 lns1) (IsBlock w2 lns2) =
   Block (w1 + w2 + if addSpace then 1 else 0) $
-     zipWith (\l1 l2 -> pad w1 l1 ++ l2) lns1' (map sp lns2')
+     zipWith (\l1 l2 -> pad w1 l1 <> l2) lns1' (map sp lns2')
     where (lns1', lns2') = case (length lns1, length lns2) of
                                 (x, y) | x > y -> (lns1,
                                                    lns2 ++ replicate (x - y) "")
                                        | x < y -> (lns1 ++ replicate (y - x) "",
                                                    lns2)
                                        | otherwise -> (lns1, lns2)
-          pad n s = s ++ replicate (n - realLength s) ' '
+          pad n s = s <> T.replicate (n - realLength s) " "
           sp "" = ""
-          sp xs = if addSpace then ' ' : xs else xs
+          sp xs = if addSpace then (" " <> xs) else xs
 
-offsetOf :: D String -> Int
+offsetOf :: D a -> Int
 offsetOf (Text o _)    = o
 offsetOf (Block w _)   = w
 offsetOf BreakingSpace = 1
@@ -393,16 +401,17 @@ offsetOf _             = 0
 
 -- | A literal string.
 text :: IsString a => String -> D a
-text [] = mempty
+text "" = mempty
 text s = case break (=='\n') s of
-           ([], _:ys) -> NewLine <> text ys
-           (xs, _:ys) -> Text (realLength xs) (fromString xs) <>
-                             (NewLine <> text ys)
-           (xs, [])   -> Text (realLength xs) (fromString xs)
+           ("", "")   -> Empty
+           (xs, "")   -> Text (realLength' xs) (fromString xs)
+           ("", _:ys) -> NewLine <> text ys
+           (xs, _:ys) -> Text (realLength' xs) (fromString xs) <>
+                           NewLine <> text ys
 
 -- | A character.
 char :: IsString a => Char -> D a
-char c = text [c]
+char c = text $ fromString [c]
 
 -- | A breaking (reflowable) space.
 space :: D a
@@ -426,21 +435,21 @@ blanklines n = BlankLines n
 -- | Uses the specified string as a prefix for every line of
 -- the inside document (except the first, if not at the beginning
 -- of the line).
-prefixed :: String -> D a -> D a
-prefixed pref doc = Prefixed pref doc
+prefixed :: IsString a => String -> D a -> D a
+prefixed pref doc = Prefixed (fromString pref) doc
 
 -- | Makes a 'Doc' flush against the left margin.
 flush :: D a -> D a
 flush doc = Flush doc
 
 -- | Indents a 'Doc' by the specified number of spaces.
-nest :: Int -> D a -> D a
+nest :: IsString a => Int -> D a -> D a
 nest ind = prefixed (replicate ind ' ')
 
 -- | A hanging indent. @hang ind start doc@ prints @start@,
 -- then @doc@, leaving an indent of @ind@ spaces on every
 -- line but the first.
-hang :: Int -> D a -> D a -> D a
+hang :: IsString a => Int -> D a -> D a -> D a
 hang ind start doc = start <> nest ind doc
 
 -- | @beforeNonBlank d@ conditionally includes @d@ unless it is
@@ -456,49 +465,49 @@ nowrap doc = mconcat . map replaceSpace . unfoldD $ doc
 
 -- | Content to print only if it comes at the beginning of a line,
 -- to be used e.g. for escaping line-initial `.` in roff man.
-afterBreak :: a -> D a
-afterBreak d = AfterBreak d
+afterBreak :: Text -> D a
+afterBreak t = AfterBreak t
 
 -- | Returns the width of a 'Doc'.
-offset :: D String -> Int
-offset d = maximum (0: map realLength (lines $ render Nothing d))
+offset :: D Text -> Int
+offset d = maximum (0: map realLength (T.lines $ render Nothing d))
 
 -- | Returns the minimal width of a 'Doc' when reflowed at breakable spaces.
-minOffset :: D String -> Int
-minOffset d = maximum (0: map realLength (lines $ render (Just 0) d))
+minOffset :: D Text -> Int
+minOffset d = maximum (0: map realLength (T.lines $ render (Just 0) d))
 
 -- | @lblock n d@ is a block of width @n@ characters, with
 -- text derived from @d@ and aligned to the left.
-lblock :: Int -> D String -> D String
+lblock :: Int -> D Text -> D Text
 lblock = block id
 
 -- | Like 'lblock' but aligned to the right.
-rblock :: Int -> D String -> D String
-rblock w = block (\s -> replicate (w - realLength s) ' ' <> s) w
+rblock :: Int -> D Text -> D Text
+rblock w = block (\s -> T.replicate (w - realLength s) " " <> s) w
 
 -- | Like 'lblock' but aligned centered.
-cblock :: Int -> D String -> D String
-cblock w = block (\s -> replicate ((w - realLength s) `div` 2) ' ' <> s) w
+cblock :: Int -> D Text -> D Text
+cblock w = block (\s -> T.replicate ((w - realLength s) `div` 2) " " <> s) w
 
 -- | Returns the height of a block or other 'Doc'.
-height :: D String -> Int
-height = length . lines . render Nothing
+height :: D Text -> Int
+height = length . T.lines . render Nothing
 
-block :: (String -> String) -> Int -> D String -> D String
+block :: (Text -> Text) -> Int -> D Text -> D Text
 block filler width d
   | width < 1 && not (isEmpty d) = block filler 1 d
   | otherwise                    = Block width $ map filler
                                  $ chop width $ render (Just width) d
 
-chop :: Int -> String -> [String]
-chop _ [] = []
-chop n cs = case break (=='\n') cs of
+chop :: Int -> Text -> [Text]
+chop _ "" = []
+chop n cs = case T.break (=='\n') cs of
                   (xs, ys)     -> if len <= n
                                      then case ys of
-                                             []     -> [xs]
-                                             ['\n'] -> [xs]
-                                             (_:zs) -> xs : chop n zs
-                                     else take n xs : chop n (drop n xs ++ ys)
+                                             ""     -> [xs]
+                                             "\n"   -> [xs]
+                                             _      -> xs : chop n (T.tail ys)
+                                     else T.take n xs : chop n (T.drop n xs <> ys)
                                    where len = realLength xs
 
 -- | Encloses a 'Doc' inside a start and end 'Doc'.
@@ -573,5 +582,8 @@ charWidth c =
 
 -- | Get real length of string, taking into account combining and double-wide
 -- characters.
-realLength :: String -> Int
-realLength = foldl' (+) 0 . map charWidth
+realLength :: Text -> Int
+realLength = T.foldl' (\tot c -> tot + charWidth c) 0
+
+realLength' :: String -> Int
+realLength' = foldr (\c tot -> tot + charWidth c) 0
